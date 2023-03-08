f974a6a2a7af696807d1cd9707bc9a276222e58e
Trav563 <trav563@gmail.com>
2023-03-08 16:13:57 -0500

    Solve problems meant for test driven development

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, num){
+    if(num < 0){
+        return `ERROR`
+    }
+    let string = ""
+    for(i = 0; i < num; i++){
+        string += word
+    }
+    return string;
+}
+;
 
 // Do not edit below this line
 module.exports = repeatString;
