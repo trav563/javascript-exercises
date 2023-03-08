86dcf3b37dd9925ede9db555566c7cc75a7bf156
Trav563 <trav563@gmail.com>
2023-03-08 16:33:46 -0500

    Complete reverseString test driven development

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverseString = splitString.reverse();
+    let joinString = reverseString.join("")
+    return joinString;
 };
 
 // Do not edit below this line
