bf372d7a36d1449255079f10b26125be65613a0d
Trav563 <trav563@gmail.com>
2023-03-09 15:28:59 -0500

    complete exercise 4 for test driven development

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(num, ...args) {
+    const array = []
 
+    num.forEach((item) => {
+        if (!args.includes(item)) {
+            array.push(item);
+          }
+        
+    });
+
+    return array;
 };
 
 // Do not edit below this line
